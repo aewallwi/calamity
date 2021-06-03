00a37c839ea847f53bee21d96af84f539375f06a
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-06-02 19:14:11 -0700

    add some fitting info tests.

--- calamity/tests/test_calamity.py
+++ calamity/tests/test_calamity.py
@@ -31,10 +31,14 @@ def test_calibrate_and_model_dpss(uvdata, sky_model, gains):
                                                                                         use_redundancy=use_redundancy, maxsteps=300)
         assert np.sqrt(np.mean(np.abs(model.data_array) ** 2.)) >= 1e3 * np.sqrt(np.mean(np.abs(resid.data_array) ** 2.))
         assert np.sqrt(np.mean(np.abs(uvdata.data_array) ** 2.)) >= 1e3 * np.sqrt(np.mean(np.abs(resid.data_array) ** 2.))
-
+        assert len(fitting_info) == 1
+        assert len(fitting_info[0]) == 1
+        
         # test that calibrating with a perfect sky model and only optimizing gains yields gains that are nearly unity.
         model, resid, filtered, gains, fitting_info = calamity.calibrate_and_model_dpss(min_dly=2.0/.3, uvdata=uvdata, gains=gains, verbose=True,
                                                                                         use_redundancy=use_redundancy, sky_model=sky_model,
                                                                                         freeze_model=True, maxsteps=300)
         assert np.sqrt(np.mean(np.abs(model.data_array) ** 2.)) >= 1e3 * np.sqrt(np.mean(np.abs(resid.data_array) ** 2.))
         assert np.allclose(gains.gain_array, 1., rtol=0., atol=1e-4)
+        assert len(fitting_info) == 1
+        assert len(fitting_info[0]) == 1
