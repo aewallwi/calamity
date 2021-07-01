f44ecb8247d7e9bc9f82dcb4faa9658a6092ad26
Aaron Ewall-Wice <aaronew@berkeley.edu>
2021-06-30 21:58:02 -0700

    prune redundant functions.

--- calamity/modeling.py
+++ calamity/modeling.py
@@ -244,123 +244,6 @@ def get_uv_overlapping_grps_conjugated(
     return fitting_grps, fitting_vec_centers, connections, grp_labels
 
 
-def simple_cov_matrix(blvecs, ant_dly, freqs, dtype=np.float32, use_tensorflow=False):
-    """Compute simple covariance matrix for subset of baselines in uvdata
-
-    Parameters
-    ----------
-    blvecs: list of np.ndarrays
-      list of length-3 np.ndarrays representing ENH baseline vectors.
-    ant_dly: float
-        intrinsic chromaticity of each antenna element.
-    freqs: array-like
-        array of frequencies (Hz) sampled by interferometer.
-    dtype: numpy.dtype, optional
-        data type to process and store covariance matrix as.
-        default is np.float32
-    use_tensorflow: bool, optional
-        if True, use tensorflow module sto compute covariance matrix.
-        Only recommended for machines with GPUs.
-        default is False.
-
-    Returns
-    -------
-    cmat: tf.Tensor object (if use_tensorflow is True) or np.ndarray(use_tensorflow is False)
-        (Nbls * Nfreqs) x (Nbls * Nfreqs) tf.Tensor object or np.ndarray with dtype dtype
-
-    """
-
-    nbls = len(blvecs)
-    uvws = np.asarray(blvecs, dtype=dtype)
-    freqs = np.asarray(freqs, dtype=dtype)
-    nfreqs = len(freqs)
-    if use_tensorflow:
-        uvws = tf.convert_to_tensor(uvws, dtype=dtype)
-        freqs = tf.convert_to_tensor(freqs, dtype)
-
-    absdiff = np.zeros((nbls * nfreqs, nbls * nfreqs), dtype=dtype)
-    if use_tensorflow:
-        tf.convert_to_tensor(absdiff, dtype=dtype)
-    for uvw_ind in range(3):
-        if use_tensorflow:
-            uvw_coord = tf.reshape(tf.experimental.numpy.outer(uvws[:, uvw_ind], freqs / 3e8), (nbls * nfreqs,))
-            cg0, cg1 = tf.meshgrid(uvw_coord, uvw_coord, indexing="ij")
-            absdiff += np.abs(cg0 - cg1) ** 2.0
-        else:
-            uvw_coord = np.reshape(np.outer(uvws[:, uvw_ind], freqs / 3e8), (nbls * nfreqs,))
-            cg0, cg1 = np.meshgrid(uvw_coord, uvw_coord, indexing="ij")
-            absdiff += np.abs(cg0 - cg1) ** 2.0
-
-    absdiff = absdiff ** 0.5
-    if use_tensorflow:
-        cmat = tf.experimental.numpy.sinc(2 * absdiff)
-    else:
-        cmat = np.sinc(2 * absdiff)
-    del absdiff
-    if use_tensorflow:
-        fvals = tf.reshape(tf.experimental.numpy.outer(tf.ones(nbls, dtype=dtype), freqs), (nbls * nfreqs,))
-        fg0, fg1 = tf.meshgrid(fvals, fvals, indexing="ij")
-        cmat = cmat * tf.experimental.numpy.sinc(2 * tf.abs(fg0 - fg1) * ant_dly)
-    else:
-        fvals = np.reshape(np.outer(tf.ones(nbls, dtype=dtype), freqs), (nbls * nfreqs,))
-        fg0, fg1 = np.meshgrid(fvals, fvals, indexing="ij")
-        cmat = cmat * np.sinc(2 * np.abs(fg0 - fg1) * ant_dly)
-
-    del fg0, fg1
-    return cmat
-
-
-def yield_simple_multi_baseline_model_comps(
-    blvecs, ant_dly, freqs, dtype=np.float32, eigenval_cutoff=1e-10, verbose=False, use_tensorflow=False
-):
-    """Generate model components for multiple baselines.
-
-    Parameters
-    ----------
-    blvecs: list of len-3 numpy.ndarrays
-      list of baseline vectors ENH
-    ant_dly: float,
-      intrinsic delay of antenna (ns).
-    freqs: array-like
-      array of frequencies sampled by interferometer (MHz).
-    dtype: numpy.dtype, optional
-      data-type to use for eigenvalue decomposition
-      default is np.float32
-    eigenval_cutoff: float, optional
-        threshold of eigenvectors to include in modeling components.
-    verbose: bool, optional
-        text outputs
-        default is False.
-    use_tensorflow: bool, optional
-        if True, use tensorflow for multi-baseline modeling matrix operations.
-        Recommended only for machines with GPUs.
-
-    Returns
-    -------
-    multi_baseline_model_comps: np.ndarray
-        (Nbl * Nfreqs) x Ncomponents numpy.ndarray containing eigenvectors
-        with eigenvalues above cutoff threshold.
-
-    """
-    cmat = simple_cov_matrix(blvecs, ant_dly, freqs, dtype=dtype, use_tensorflow=use_tensorflow)
-    # get eigenvectors
-    echo(
-        f"{datetime.datetime.now()} Deriving modeling components with eigenvalue decomposition...\n",
-        verbose=verbose,
-    )
-    if use_tensorflow:
-        evals, evecs = tf.linalg.eigh(cmat)
-        evals = evals.numpy()
-        evecs = evecs.numpy()
-    else:
-        evals, evecs = np.linalg.eigh(cmat)
-
-    selection = evals / evals.max() >= eigenval_cutoff
-    evals = evals[selection][::-1]
-    evecs = evecs[:, selection][:, ::-1]
-    return evecs
-
-
 def yield_dpss_model_comps_bl_grp(
     length,
     freqs,
@@ -481,7 +364,9 @@ def yield_mixed_comps(
     fitting_blvecs,
     freqs,
     eigenval_cutoff=1e-10,
-    ant_dly=0.0,
+    min_dly=0.0,
+    horizon=1.0,
+    offset=0.0,
     verbose=False,
     dtype=np.float32,
     notebook_progressbar=False,
--- calamity/simple_cov.py
+++ calamity/simple_cov.py
@@ -4,7 +4,9 @@ import datetime
 from .utils import echo
 
 
-def simple_cov_matrix(uvdata, baseline_group, ant_dly, dtype=np.float32):
+def simple_cov_matrix(
+    uvdata, baseline_group, ant_dly=0.0, horizon=1.0, offset=0.0, min_dly=0.0, dtype=np.float32, use_tensorflow=False
+):
     """Compute simple covariance matrix for subset of baselines in uvdata
 
     Parameters
@@ -15,14 +17,30 @@ def simple_cov_matrix(uvdata, baseline_group, ant_dly, dtype=np.float32):
         tuple of tuple of 2-tuples where each tuple is a different redundant baseline
         group that contains 2-tuples specifying each baseline.
     ant_dly: float
-        intrinsic chromaticity of each antenna element.
+        intrinsic chromaticity of each antenna element, manifested in a
+        multiplicative sinc matrix sinc(2 pi tau_ant * (nu_1 - nu_0))
+    horizon: float, optional
+        fraction of horizon for beam to extend to in analytic cov matrix.
+        default is 1.0
+    offset: float, optional
+        additional offset causing additional decorrelation between antennas
+        (units of ns).
+        default is 0.0
+    min_dly: float, optional
+        minimum decorrelation delay between points in uv plane. Net effect should
+        be some additional modes and signal loss.
+        default is 0.0
     dtype: numpy.dtype
         data type to process and store covariance matrix as.
+    use_tensorflow: bool, optional
+        if True, use tensorflow for multi-baseline modeling matrix operations.
+        Recommended only for machines with GPUs.
 
     Returns
     -------
-    cmat: tf.Tensor object
-        (Nbls * Nfreqs) x (Nbls * Nfreqs) tf.Tensor object
+    cmat: tf.Tensor object (if use_tensorflow is True) or np.ndarray(use_tensorflow is False)
+        (Nbls * Nfreqs) x (Nbls * Nfreqs) tf.Tensor object or np.ndarray with dtype dtype
+
 
     """
     ap_data = set(uvdata.get_antpairs())
@@ -36,58 +54,123 @@ def simple_cov_matrix(uvdata, baseline_group, ant_dly, dtype=np.float32):
             dinds = uvdata.antpair2ind(red_grp[0][::-1])
             uvws.extend(-uvdata.uvw_array[dinds])
     # find differences between all uvws
-    uvws = np.asarray(uvws)
+    uvws = np.asarray(uvws, dtype=dtype)
     nbls = len(uvws)
-    uvws = tf.convert_to_tensor(uvws, dtype=dtype)
-    freqs = tf.convert_to_tensor(np.asarray(uvdata.freq_array.squeeze()), dtype)
-    absdiff = tf.convert_to_tensor(np.zeros((nbls, nbls), dtype=dtype), dtype=dtype)
+    freqs = np.asarray(uvdata.freq_array.squeeze(), dtype=dtype)
+    absdiff = np.zeros((nbls, nbls), dtype=dtype)
+    if use_tensorflow:
+        freqs = tf.convert_to_tensor(freqs, dtype)
+        absdiff = tf.convert_to_tensor(basdiff, dtype=dtype)
     for uvw_ind in range(3):
-        uvw_coord = tf.reshape(tf.outer(uvws[:, uvw_ind], freqs / 3e8), (nbls * uvdata.Nfreqs,))
-        cg0, cg1 = tf.meshgrid(uvw_coord, uvw_coord, indexing="ij")
-        absdiff += np.abs(cg0 - cg1) ** 2.0
-    absdiff = absdiff ** 0.5
-    cmat = tf.experimental.numpy.sinc(2 * absdiff)
-    del absdiff
-    fg0, fg1 = tf.reshape(tf.outer(tf.ones(nbls, dytpe=dtype), freqs), (nbls * uvdata.Nfreqs,), indexing="ij")
-    cmat = cmat * tf.experimental.sinc(2 * tf.abs(fg0 - fg1) * ant_dly)
+        if use_tensorflow:
+            uvw_coord = tf.reshape(tf.outer(uvws[:, uvw_ind], freqs / 3e8), (nbls * uvdata.Nfreqs,))
+            cg0, cg1 = tf.meshgrid(uvw_coord, uvw_coord, indexing="ij")
+            absdiff += tf.math.square(tf.math.abs(cg0 - cg1) * horizon)
+        else:
+            uvw_coord = np.reshape(np.outer(uvws[:, uvw_ind], freqs / 3e8), (nbls * nfreqs,))
+            cg0, cg1 = np.meshgrid(uvw_coord, uvw_coord, indexing="ij")
+            absdiff += np.abs(cg0 - cg1) ** 2.0
+    if use_tensorflow:
+        absdiff = tf.math.sqrt(absdiff) * horizon
+        fg0, fg1 = tf.reshape(tf.outer(tf.ones(nbls, dytpe=dtype), freqs), (nbls * uvdata.Nfreqs,), indexing="ij")
+        dfg = tf.abs(fg0 - fg1)
+    else:
+        absdiff = np.sqrt(absdiff) * horizon
+        fg0, fg1 = np.meshgrid(fvals, fvals, indexing="ij")
+        dfg = np.abs(fg0 - fg1)
+
     del fg0, fg1
+    absdiff += dfg * offset / 1e9
+    if use_tensorflow:
+        cmat = tf.experimental.numpy.sinc(2 * tf.maximum(absdiff, min_dly * dfg / 1e9))
+    else:
+        cmat = np.sinc(2 * np.maximum(min_dly * dfg / 1e9, absdiff))
+    del absdiff
+    if use_tensorflow:
+        cmat = cmat * tf.experimental.numpy.sinc(2 * dfg * ant_dly)
+    else:
+        cmat = cmat * np.sinc(2 * dfg * ant_dly)
+    del dfg
     return cmat
 
 
-def yield_simple_multi_baseline_model_comps(uvdata, baseline_group, ant_dly, dtype=np.float32, verbose=False):
+def yield_simple_multi_baseline_model_comps(
+    blvecs,
+    freqs,
+    ant_dly=0.0,
+    horizon=1.0,
+    offset=0.0,
+    min_dly=0.0,
+    dtype=np.float32,
+    verbose=False,
+    use_tensorflow=False,
+    eigenval_cutoff=1e-10,
+):
     """Generate model components for multiple baselines.
 
     Parameters
     ----------
-    uvdata: UVdata object
-        uvdata holding data that you will be modeling
-    antpairs: tuple of tuples of 2-tuples
-        tuple of tuples where each sub-tuple is a redundant group
-        that you will be jointly modeling which in turn contains
-        2-tuples representing antpairs in each redundant group
+    blvecs: list of len-3 numpy.ndarrays
+      list of baseline vectors ENH
+    ant_dly: float, optional
+        intrinsic chromaticity of each antenna element, manifested in a
+        multiplicative sinc matrix sinc(2 pi tau_ant * (nu_1 - nu_0))
+        default is 0.
+    freqs: array-like
+      array of frequencies sampled by interferometer (MHz).
+    horizon: float, optional
+        fraction of horizon for beam to extend to in analytic cov matrix.
+        default is 1.0
+    offset: float, optional
+        additional offset causing additional decorrelation between antennas
+        (units of ns).
+        default is 0.0
+    min_dly: float, optional
+        minimum decorrelation delay between points in uv plane. Net effect should
+        be some additional modes and signal loss.
+        default is 0.0
     dtype: numpy.dtype, optional
         data-type to use for eigenvalue decomposition
         default is np.float32
     verbose: bool, optional
         text outputs
         default is False.
+    use_tensorflow: bool, optional
+        if True, use tensorflow for multi-baseline modeling matrix operations.
+        Recommended only for machines with GPUs.
+    eigenval_cutoff: float, optional
+        threshold of eigenvectors to include in modeling components.
 
     Returns
     -------
-    multi_baseline_model_comps: dict
-        dict with baseline_group as single key and
-        (Nbl * Nfreqs) x Ncomponents tf.Tensor array as single value
+    multi_baseline_model_comps: np.ndarray
+        (Nbl * Nfreqs) x Ncomponents numpy.ndarray containing eigenvectors
+        with eigenvalues above cutoff threshold.
 
     """
-    cmat = simple_cov_matrix(uvdata, baseline_group, ant_dly, dtype=dtype, verbose=verbose)
+    cmat = simple_cov_matrix(
+        blvecs,
+        ant_dly=ant_dly,
+        horizon=horizon,
+        offset=offset,
+        min_dly=min_dly,
+        dtype=dtype,
+        verbose=verbose,
+        use_tensorflow=use_tensorflow,
+    )
     # get eigenvectors
     echo(
         f"{datetime.datetime.now()} Deriving modeling components with eigenvalue decomposition...\n",
         verbose=verbose,
     )
-    evals, evecs = tf.linalg.eigh(cmat)
-    evals_n = evals.numpy()
-    selection = evals_n / evals_n.max() >= evals_n
+    if use_tensorflow:
+        evals, evecs = tf.linalg.eigh(cmat)
+        evals = evals.numpy()
+        evecs = evecs.numpy()
+    else:
+        evals, evecs = np.linalg.eigh(cmat)
+
+    selection = evals / evals.max() >= eigenval_cutoff
     evals = evals[selection][::-1]
     evecs = evecs[:, selection][:, ::-1]
-    return {baseline_group: evecs.numpy()}
+    return evecs
